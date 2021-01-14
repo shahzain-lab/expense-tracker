d8bbfc8f221f3505647e3896531c301feaf23d3d
shahzain <69709547+shahzain-lab@users.noreply.github.com>
2021-01-13 16:01:33 -0800

    links added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div>
-      <div>
-        
+    <div className="mainDiv">
+      <h3>expense tracker</h3>
+      <div className="section">
+
       </div>
     </div>
   );
