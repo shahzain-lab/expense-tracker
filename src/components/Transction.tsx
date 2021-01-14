45be785c22d0ce846fcd3edbdd9589540138294d
shahzain <69709547+shahzain-lab@users.noreply.github.com>
2021-01-14 02:12:18 -0800

    updated

--- src/components/Transction.tsx
+++ src/components/Transction.tsx
@@ -8,12 +8,13 @@ interface Props{
 }
 
 export const Transction: FC<Props> = ({transaction}) => {
-    
+    const sign = transaction.amount > 0 ? '+' : '-';
+    const TransTypes = transaction.amount > 0 ? 'plus' : 'minus';
     return (
         <div>
             <li className="Transction">
                 {transaction.description}
-    <span>{transaction.amount}</span>
+    <span>{sign} ${Math.abs(transaction.amount)}</span>
     <i className="fas fa-trash-alt"></i>
             </li>
         </div>
