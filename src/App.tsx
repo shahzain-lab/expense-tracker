23ef7dd84d71cfd57e3afee3a2379c1a077d7c17
shahzain <69709547+shahzain-lab@users.noreply.github.com>
2021-01-13 17:04:16 -0800

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import { Summary } from './components/Summary';
+import { AddTrans } from './components/AddTrans';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <h3>expense tracker</h3>
       <div className="section">
         <Summary />
+        <AddTrans />
       </div>
       </div>
     </div>
--- src/components/AddTrans.tsx
+++ src/components/AddTrans.tsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import './style.css'
 
 export const AddTrans = () => {
     return (
-        <div>
-            <h3>Add Transction</h3>
+        <div className="AddTrans">
+            <h3 className="Addhead">Add Transction</h3>
+            <form>
+                <label>Add Amount</label>
+                <br />
+                <input 
+                 type="number"
+                 placeholder="Add Transction amount"
+                />
+                <br />
+                <label>Add Description</label>
+                <br />
+                <input 
+                 type="text"
+                 placeholder="Add Amount Description"
+                />
+                <br />
+                <input 
+                  type="submit" 
+                  value="submit"
+                />
+            </form>
         </div>
     )
 }
