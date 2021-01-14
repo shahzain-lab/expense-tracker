e4710de30906b223e83ad43872d1b243e606f985
shahzain <69709547+shahzain-lab@users.noreply.github.com>
2021-01-13 16:25:36 -0800

    created balance com

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
+import { Balance } from './components/Balance';
 
 function App() {
   return (
     <div className="mainDiv">
+      <div>
       <h3>expense tracker</h3>
       <div className="section">
-
+          <Balance />
+      </div>
       </div>
     </div>
   );
---
+++ src/components/Balance.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './style.css'
+
+export const Balance = () => {
+    return (
+        <div className="Balance">
+            <h4>Balance <span>$00,00</span></h4>
+        </div>
+    )
+}
